2173aaec4c8ba353c1b428b714193548c6b1aca9
Dave Henton <davehenton@gmail.com>
2016-08-15 02:56:41 -0700

    print * to debug token issue

--- app/helpers/query_helpers.rb
+++ app/helpers/query_helpers.rb
@@ -1,10 +1,14 @@
 helpers do
 	def query_top_tracks(token_code)
 		token_response = HTTParty.post("https://accounts.spotify.com/api/token", body: { grant_type:"authorization_code", code: token_code, redirect_uri: "http://localhost:9393/users/landing", client_id: ENV['SPOTIFY_API_KEY'], client_secret: ENV['SPOTIFY_CLIENT_SECRET'] }, header: { authorization: "code" })
+		p "*" * 50 
 		p token_response
+		p "*" * 50 
 		token = token_response["access_token"]
 		p token
-		my_top = HTTParty.get("https://api.spotify.com/v1/me/top/tracks?limit=2", headers: { "Authorization" => "Bearer " + token})
+		p "*" * 50 
+		my_top = HTTParty.get("https://api.spotify.com/v1/me/top/tracks?limit=2", headers: { "Authorization" => "Bearer " + "token" })
+		my_top = HTTParty.get("https://api.spotify.com/v1/me/top/tracks?limit=2", headers: { "Authorization" => "Bearer " + token })
 	    return [my_top["items"][0]["artists"][0]["name"], my_top["items"][0]["name"], my_top["items"][1]["artists"][0]["name"], my_top["items"][1]["name"]]
 	end
 
