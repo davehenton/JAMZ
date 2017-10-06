ecec453a84246d63613c12dad2a69ef1489b1cbb
Dave Henton <dave@codeclimate.com>
2017-10-06 15:43:00 -0400

    duplicated.rb

---
+++ app/controllers/duplicated.rb
@@ -0,0 +1,17 @@
+def describe
+  first_name + " " + middle_name + " " + last name + " (" + age + years old), lives
+end
+
+def describe
+  first_name + " " + middle_name + " " + last name + " (" + age + years old), lives
+end
+
+
+def describe
+  first_name + " " + middle_name + " " + last name + " (" + age + years old), lives
+end
+
+
+def describe
+  first_name + " " + middle_name + " " + last name + " (" + age + years old), lives
+end
