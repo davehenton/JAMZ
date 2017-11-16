392957776f12e1a7d64d04e4cef5445ba3e38e12
Dave Henton <dave@codeclimate.com>
2017-11-16 17:01:35 -0500

    Update query_helpers.rb

--- features/query_helpers.rb
+++ features/query_helpers.rb
@@ -32,3 +32,5 @@ def query_top_tracks(token_code)
 		my_top = HTTParty.get("https://api.spotify.com/v1/me/top/tracks?limit=2", headers: { "Authorization" => "Bearer " + token })
 		return [my_top["items"][0]["artists"][0]["name"], my_top["items"][0]["name"], my_top["items"][1]["artists"][0]["name"], my_top["items"][1]["name"]]
 	end
+
+puts
