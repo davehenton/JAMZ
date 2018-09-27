8b75546a74f07741411142e5c2cd7fe72ebd5e18
Dave Henton <dave@codeclimate.com>
2018-09-27 11:39:39 -0400

    update
    test out PR comments

--- test.rb
+++ test.rb
@@ -7,3 +7,53 @@
 'in this case "single quotes" is OK'
 
 "i wonder how it feels about #{'this'}"
+
+# This file can be removed. Hopefully Henrik will remove it soon after committing it.
+
+"hello world"
+
+'single quotes forever'
+
+'in this case "single quotes" is OK'
+
+"i wonder how it feels about #{'this'}"
+
+# This file can be removed. Hopefully Henrik will remove it soon after committing it.
+
+"hello world"
+
+'single quotes forever'
+
+'in this case "single quotes" is OK'
+
+"i wonder how it feels about #{'this'}"
+
+# This file can be removed. Hopefully Henrik will remove it soon after committing it.
+
+"hello world"
+
+'single quotes forever'
+
+'in this case "single quotes" is OK'
+
+"i wonder how it feels about #{'this'}"
+
+# This file can be removed. Hopefully Henrik will remove it soon after committing it.
+
+"hello world"
+
+'single quotes forever'
+
+'in this case "single quotes" is OK'
+
+"i wonder how it feels about #{'this'}"
+
+# This file can be removed. Hopefully Henrik will remove it soon after committing it.
+
+"hello world"
+
+'single quotes forever'
+
+'in this case "single quotes" is OK'
+
+"i wonder how it feels about #{'this'}"
