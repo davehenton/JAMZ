615bdc9f145572bac22924c8341dc4660fd8055a
Dave Henton <dave@codeclimate.com>
2019-03-27 17:41:06 -0600

    Update duplicated.rb

--- app/controllers/duplicated.rb
+++ app/controllers/duplicated.rb
@@ -13,3 +13,618 @@ end
 def describe
   first_name +  middle_name + last_name
 end
+def describe
+  first_name +  middle_name + last_name
+end
+
+def describe
+  first_name +  middle_name + last_name
+end
+
+def describe
+  first_name +  middle_name + last_name
+end
+
+def describe
+  first_name +  middle_name + last_name
+end
+def describe
+  first_name +  middle_name + last_name
+end
+
+def describe
+  first_name +  middle_name + last_name
+end
+
+def describe
+  first_name +  middle_name + last_name
+end
+
+def describe
+  first_name +  middle_name + last_name
+end
+def describe
+  first_name +  middle_name + last_name
+end
+
+def describe
+  first_name +  middle_name + last_name
+end
+
+def describe
+  first_name +  middle_name + last_name
+end
+
+def describe
+  first_name +  middle_name + last_name
+end
+def describe
+  first_name +  middle_name + last_name
+end
+
+def describe
+  first_name +  middle_name + last_name
+end
+
+def describe
+  first_name +  middle_name + last_name
+end
+
+def describe
+  first_name +  middle_name + last_name
+end
+def describe
+  first_name +  middle_name + last_name
+end
+
+def describe
+  first_name +  middle_name + last_name
+end
+
+def describe
+  first_name +  middle_name + last_name
+end
+
+def describe
+  first_name +  middle_name + last_name
+end
+def describe
+  first_name +  middle_name + last_name
+end
+
+def describe
+  first_name +  middle_name + last_name
+end
+
+def describe
+  first_name +  middle_name + last_name
+end
+
+def describe
+  first_name +  middle_name + last_name
+end
+def describe
+  first_name +  middle_name + last_name
+end
+
+def describe
+  first_name +  middle_name + last_name
+end
+
+def describe
+  first_name +  middle_name + last_name
+end
+
+def describe
+  first_name +  middle_name + last_name
+end
+def describe
+  first_name +  middle_name + last_name
+end
+
+def describe
+  first_name +  middle_name + last_name
+end
+
+def describe
+  first_name +  middle_name + last_name
+end
+
+def describe
+  first_name +  middle_name + last_name
+end
+def describe
+  first_name +  middle_name + last_name
+end
+
+def describe
+  first_name +  middle_name + last_name
+end
+
+def describe
+  first_name +  middle_name + last_name
+end
+
+def describe
+  first_name +  middle_name + last_name
+end
+def describe
+  first_name +  middle_name + last_name
+end
+
+def describe
+  first_name +  middle_name + last_name
+end
+
+def describe
+  first_name +  middle_name + last_name
+end
+
+def describe
+  first_name +  middle_name + last_name
+end
+def describe
+  first_name +  middle_name + last_name
+end
+
+def describe
+  first_name +  middle_name + last_name
+end
+
+def describe
+  first_name +  middle_name + last_name
+end
+
+def describe
+  first_name +  middle_name + last_name
+end
+def describe
+  first_name +  middle_name + last_name
+end
+
+def describe
+  first_name +  middle_name + last_name
+end
+
+def describe
+  first_name +  middle_name + last_name
+end
+
+def describe
+  first_name +  middle_name + last_name
+end
+def describe
+  first_name +  middle_name + last_name
+end
+
+def describe
+  first_name +  middle_name + last_name
+end
+
+def describe
+  first_name +  middle_name + last_name
+end
+
+def describe
+  first_name +  middle_name + last_name
+end
+def describe
+  first_name +  middle_name + last_name
+end
+
+def describe
+  first_name +  middle_name + last_name
+end
+
+def describe
+  first_name +  middle_name + last_name
+end
+
+def describe
+  first_name +  middle_name + last_name
+end
+def describe
+  first_name +  middle_name + last_name
+end
+
+def describe
+  first_name +  middle_name + last_name
+end
+
+def describe
+  first_name +  middle_name + last_name
+end
+
+def describe
+  first_name +  middle_name + last_name
+end
+def describe
+  first_name +  middle_name + last_name
+end
+
+def describe
+  first_name +  middle_name + last_name
+end
+
+def describe
+  first_name +  middle_name + last_name
+end
+
+def describe
+  first_name +  middle_name + last_name
+end
+def describe
+  first_name +  middle_name + last_name
+end
+
+def describe
+  first_name +  middle_name + last_name
+end
+
+def describe
+  first_name +  middle_name + last_name
+end
+
+def describe
+  first_name +  middle_name + last_name
+end
+def describe
+  first_name +  middle_name + last_name
+end
+
+def describe
+  first_name +  middle_name + last_name
+end
+
+def describe
+  first_name +  middle_name + last_name
+end
+
+def describe
+  first_name +  middle_name + last_name
+end
+def describe
+  first_name +  middle_name + last_name
+end
+
+def describe
+  first_name +  middle_name + last_name
+end
+
+def describe
+  first_name +  middle_name + last_name
+end
+
+def describe
+  first_name +  middle_name + last_name
+end
+def describe
+  first_name +  middle_name + last_name
+end
+
+def describe
+  first_name +  middle_name + last_name
+end
+
+def describe
+  first_name +  middle_name + last_name
+end
+
+def describe
+  first_name +  middle_name + last_name
+end
+def describe
+  first_name +  middle_name + last_name
+end
+
+def describe
+  first_name +  middle_name + last_name
+end
+
+def describe
+  first_name +  middle_name + last_name
+end
+
+def describe
+  first_name +  middle_name + last_name
+end
+def describe
+  first_name +  middle_name + last_name
+end
+
+def describe
+  first_name +  middle_name + last_name
+end
+
+def describe
+  first_name +  middle_name + last_name
+end
+
+def describe
+  first_name +  middle_name + last_name
+end
+def describe
+  first_name +  middle_name + last_name
+end
+
+def describe
+  first_name +  middle_name + last_name
+end
+
+def describe
+  first_name +  middle_name + last_name
+end
+
+def describe
+  first_name +  middle_name + last_name
+end
+def describe
+  first_name +  middle_name + last_name
+end
+
+def describe
+  first_name +  middle_name + last_name
+end
+
+def describe
+  first_name +  middle_name + last_name
+end
+
+def describe
+  first_name +  middle_name + last_name
+end
+def describe
+  first_name +  middle_name + last_name
+end
+
+def describe
+  first_name +  middle_name + last_name
+end
+
+def describe
+  first_name +  middle_name + last_name
+end
+
+def describe
+  first_name +  middle_name + last_name
+end
+def describe
+  first_name +  middle_name + last_name
+end
+
+def describe
+  first_name +  middle_name + last_name
+end
+
+def describe
+  first_name +  middle_name + last_name
+end
+
+def describe
+  first_name +  middle_name + last_name
+end
+def describe
+  first_name +  middle_name + last_name
+end
+
+def describe
+  first_name +  middle_name + last_name
+end
+
+def describe
+  first_name +  middle_name + last_name
+end
+
+def describe
+  first_name +  middle_name + last_name
+end
+def describe
+  first_name +  middle_name + last_name
+end
+
+def describe
+  first_name +  middle_name + last_name
+end
+
+def describe
+  first_name +  middle_name + last_name
+end
+
+def describe
+  first_name +  middle_name + last_name
+end
+def describe
+  first_name +  middle_name + last_name
+end
+
+def describe
+  first_name +  middle_name + last_name
+end
+
+def describe
+  first_name +  middle_name + last_name
+end
+
+def describe
+  first_name +  middle_name + last_name
+end
+def describe
+  first_name +  middle_name + last_name
+end
+
+def describe
+  first_name +  middle_name + last_name
+end
+
+def describe
+  first_name +  middle_name + last_name
+end
+
+def describe
+  first_name +  middle_name + last_name
+end
+def describe
+  first_name +  middle_name + last_name
+end
+
+def describe
+  first_name +  middle_name + last_name
+end
+
+def describe
+  first_name +  middle_name + last_name
+end
+
+def describe
+  first_name +  middle_name + last_name
+end
+def describe
+  first_name +  middle_name + last_name
+end
+
+def describe
+  first_name +  middle_name + last_name
+end
+
+def describe
+  first_name +  middle_name + last_name
+end
+
+def describe
+  first_name +  middle_name + last_name
+end
+def describe
+  first_name +  middle_name + last_name
+end
+
+def describe
+  first_name +  middle_name + last_name
+end
+
+def describe
+  first_name +  middle_name + last_name
+end
+
+def describe
+  first_name +  middle_name + last_name
+end
+def describe
+  first_name +  middle_name + last_name
+end
+
+def describe
+  first_name +  middle_name + last_name
+end
+
+def describe
+  first_name +  middle_name + last_name
+end
+
+def describe
+  first_name +  middle_name + last_name
+end
+def describe
+  first_name +  middle_name + last_name
+end
+
+def describe
+  first_name +  middle_name + last_name
+end
+
+def describe
+  first_name +  middle_name + last_name
+end
+
+def describe
+  first_name +  middle_name + last_name
+end
+def describe
+  first_name +  middle_name + last_name
+end
+
+def describe
+  first_name +  middle_name + last_name
+end
+
+def describe
+  first_name +  middle_name + last_name
+end
+
+def describe
+  first_name +  middle_name + last_name
+end
+def describe
+  first_name +  middle_name + last_name
+end
+
+def describe
+  first_name +  middle_name + last_name
+end
+
+def describe
+  first_name +  middle_name + last_name
+end
+
+def describe
+  first_name +  middle_name + last_name
+end
+def describe
+  first_name +  middle_name + last_name
+end
+
+def describe
+  first_name +  middle_name + last_name
+end
+
+def describe
+  first_name +  middle_name + last_name
+end
+
+def describe
+  first_name +  middle_name + last_name
+end
+def describe
+  first_name +  middle_name + last_name
+end
+
+def describe
+  first_name +  middle_name + last_name
+end
+
+def describe
+  first_name +  middle_name + last_name
+end
+
+def describe
+  first_name +  middle_name + last_name
+end
+def describe
+  first_name +  middle_name + last_name
+end
+
+def describe
+  first_name +  middle_name + last_name
+end
+
+def describe
+  first_name +  middle_name + last_name
+end
+
+def describe
+  first_name +  middle_name + last_name
+end
+def describe
+  first_name +  middle_name + last_name
+end
+
+def describe
+  first_name +  middle_name + last_name
+end
+
+def describe
+  first_name +  middle_name + last_name
+end
+
+def describe
+  first_name +  middle_name + last_name
+end
