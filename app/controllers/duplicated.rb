e0c4be49a3c494a6bc7cceb84f3c1a2fdee3af3b
Dave Henton <dave@codeclimate.com>
2021-02-09 12:45:53 -0600

    Update duplicated.rb
    add bug on 1291

--- app/controllers/duplicated.rb
+++ app/controllers/duplicated.rb
@@ -1288,3 +1288,4 @@ BUG
 HACK
 ## adding comment after selecting "post only once"
 ## adding comment after changing issue status for 1280 to "invalid" and statuses for 1282/1283 to "confirmed"
+BUG
