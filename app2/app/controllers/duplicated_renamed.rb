8e5a6f9e71eda0f564bffdd4b2b92c392c37f58a
Dave Henton <dave@codeclimate.com>
2023-06-29 15:01:05 -0500

    Update duplicated_renamed.rb

--- app2/app/controllers/duplicated_renamed.rb
+++ app2/app/controllers/duplicated_renamed.rb
@@ -13,3 +13,47 @@ end
 def describe
   first_name +  middle_name + last_name
 end
+def describe
+  first_name +  middle_name + last_name
+end
+
+def describe
+  first_name +  middle_name + last_name
+end
+
+def describe
+  first_name +  middle_name + last_name
+end
+
+def describe
+  first_name +  middle_name + last_name
+end
+def describe
+  first_name +  middle_name + last_name
+end
+
+def describe
+  first_name +  middle_name + last_name
+end
+
+def describe
+  first_name +  middle_name + last_name
+end
+
+def describe
+  first_name +  middle_name + last_name
+enddef describe
+  first_name +  middle_name + last_name
+end
+
+def describe
+  first_name +  middle_name + last_name
+end
+
+def describe
+  first_name +  middle_name + last_name
+end
+
+def describe
+  first_name +  middle_name + last_name
+end
