fd2c06e30fcfb92a5a5455f94e2bbc373733ab6a
Dave Henton <dave@codeclimate.com>
2024-01-25 15:46:32 -0600

    Update duplicated_renamed.rb

--- app2/app/controllers/duplicated_renamed.rb
+++ app2/app/controllers/duplicated_renamed.rb
@@ -57,3 +57,182 @@ end
 def describe
   first_name +  middle_name + last_name
 end
+def describe
+  first_name +  middle_name + last_name
+end
+
+def describe
+  first_name +  middle_name + last_name
+endasdf
+
+def describe
+  first_name +  middle_name + last_name
+end
+
+def describe
+  first_name +  middle_name + last_name
+end
+def describe
+  first_name +  middle_name + last_name
+end
+
+def describe
+  first_name +  middle_name + last_name
+end
+
+def describe
+  first_name +  middle_name + last_name
+end
+
+def describe
+  first_name +  middle_name + last_name
+end
+def describe
+  first_name +  middle_name + last_name
+end
+
+def describe
+  first_name +  middle_name + last_name
+end
+
+def describe
+  first_name +  middle_name + last_name
+end
+
+def describe
+  first_name +  middle_name + last_name
+enddef describe
+  first_name +  middle_name + last_name
+end
+
+def describe
+  first_name +  middle_name + last_name
+end
+
+def describe
+  first_name +  middle_name + last_name
+end
+
+def describe
+  first_name +  middle_name + last_name
+end
+
+  def describe
+  first_name +  middle_name + last_name
+end
+
+def describe
+  first_name +  middle_name + last_name
+endasdf
+
+def describe
+  first_name +  middle_name + last_name
+end
+
+def describe
+  first_name +  middle_name + last_name
+end
+def describe
+  first_name +  middle_name + last_name
+end
+
+def describe
+  first_name +  middle_name + last_name
+end
+
+def describe
+  first_name +  middle_name + last_name
+end
+
+def describe
+  first_name +  middle_name + last_name
+end
+def describe
+  first_name +  middle_name + last_name
+end
+
+def describe
+  first_name +  middle_name + last_name
+end
+
+def describe
+  first_name +  middle_name + last_name
+end
+
+def describe
+  first_name +  middle_name + last_name
+enddef describe
+  first_name +  middle_name + last_name
+end
+
+def describe
+  first_name +  middle_name + last_name
+end
+
+def describe
+  first_name +  middle_name + last_name
+end
+
+def describe
+  first_name +  middle_name + last_name
+end
+
+  def describe
+  first_name +  middle_name + last_name
+end
+
+def describe
+  first_name +  middle_name + last_name
+endasdf
+
+def describe
+  first_name +  middle_name + last_name
+end
+
+def describe
+  first_name +  middle_name + last_name
+end
+def describe
+  first_name +  middle_name + last_name
+end
+
+def describe
+  first_name +  middle_name + last_name
+end
+
+def describe
+  first_name +  middle_name + last_name
+end
+
+def describe
+  first_name +  middle_name + last_name
+end
+def describe
+  first_name +  middle_name + last_name
+end
+
+def describe
+  first_name +  middle_name + last_name
+end
+
+def describe
+  first_name +  middle_name + last_name
+end
+
+def describe
+  first_name +  middle_name + last_name
+enddef describe
+  first_name +  middle_name + last_name
+end
+
+def describe
+  first_name +  middle_name + last_name
+end
+
+def describe
+  first_name +  middle_name + last_name
+end
+
+def describe
+  first_name +  middle_name + last_name
+end
